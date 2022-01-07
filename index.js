9f7eb1de3b989375e3eb7b344b70bad1aab5537e
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2022-01-07 11:16:44 +0530

    First commit: create a directory structure and setup express server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.listen(port, function(err) {
+    if(err) {
+        console.log("error in returning to the server :((",err);
+    }
+    console.log("YUPP! my server is running on port: ",port);
+}) ;
