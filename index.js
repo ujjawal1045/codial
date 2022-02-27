7cd72879dd59fa2c76d8ba855e67a9836dc9602c
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2022-02-28 00:13:33 +0530

    installed express-session added middleware redirected user in controller and routed the request

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+//used for session cookies
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
 
 
 app.use(express.urlencoded());
@@ -18,17 +22,36 @@ app.set('layout extractStyles',true);
 app.set('layout extractScripts',true);
 
 
-// use express router
-app.use('/', require('./routes'));
 
 // setup the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+app.use(session({
+    name: 'coedial',
+    //todo change the secret before deployent in production mode
+    secret: 'blabla',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000*60*100)
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// use express router
+app.use('/', require('./routes'));
+
 
 
 app.listen(port, function(err) {
     if(err) {
         console.log("error in returning to the server :((",err);
     }
+
     console.log("YUPP! my server is running on port: ",port);
 }) ;
+
+
