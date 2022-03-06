e87d55e7b7e99620147d96d8bad60ad89e5f5052
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2022-03-06 16:40:46 +0530

    displayed the post and info related to user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,25 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res) {
-    console.log(req.cookies);
-    res.cookie
+    // console.log(req.cookies);
+    // res.cookie
     
-    ('user_id', 25);
+    // ('user_id', 25);
+
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Codeial | home",
+    //         posts: posts
+    //     });
+    // });
+//populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: "Codeial | home",
+            posts: posts
+        });
+    })
+    
     //return res.end('<h1> Express is up for codeial! </h1>')
-    return res.render('home', {
-        title: "home"
-    });
+    
 }
