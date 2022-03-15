f45fa751693cec1e492be8cd43ac2efaa939d768
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2022-03-16 00:13:58 +0530

    deleted a post from databse as well as view

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,7 @@
+const { remove } = require('../models/post');
 const Post = require('../models/post');
+const Comment = require('../models/comment');
+
 module.exports.create = function(req, res) {
     Post.create({
         content: req.body.content,
@@ -9,5 +12,21 @@ module.exports.create = function(req, res) {
             return;
     }
     return res.redirect('back');
+    });
+}
+
+module.exports.destroy = function(req, res) {
+    Post.findById(req.params.id, function(err, post) {
+        //.id means converting the object id to string otherwise ._id will use for number
+        if(post.user == req.user.id) {
+            post.remove();
+
+            Comment.deleteMany({post: req.params.id}, function(err) {
+                return res.redirect('back');
+            });
+
+        } else {
+            return res.redirect('back');
+        }
     });
 }
