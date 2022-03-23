dc4803795ad6b5455ad4107716e45e863381249e
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2022-03-24 00:14:02 +0530

    received the post data and able to show it without refreshing the page with help of ajax

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -11,7 +11,8 @@
                 url: '/post/create',
                 data: newPostForm.serialize(),
                 success: function(data) {
-                    console.log(data);
+                    let newPost = newPostDom(data.data.post);
+                    $('#post-list-container>ul').prepend(newPost);
 
                 }, error: function(error) {
                     console.log(error.responseText);
@@ -19,6 +20,49 @@
             });
         });
     }
+
+
+
+    //method to create a post in DOM
+
+    let newPostDom = function(post) {
+        return $(` <li id="post-${post._id}">
+                <p>
+                    
+                    <small>
+                        <a class="delete-post-button" href="/post/destroy/${post._id}"> X </a>
+                    </small>
+                    
+                    ${post.content}
+                    <br>
+                    <small>
+                        ${post.user.name }
+                    </small>
+                </p>
+                <div class="post-comments">
+                    
+                        <form action="/comments/create" method="post">
+                            <input type="text" name="content" placeholder="type here to add comment..." required>
+                            <input type="hidden" name="post" value=" ${post._id}">
+                            <input type="submit" value="Add comment"> 
+                        </form>
+                    
+            
+                    <div class="post-comment-list">
+                        <ul id="post-comments-${post._id}">
+                            
+                        </ul>
+                    </div>
+                </div>
+                
+            </li> `)
+   }
+
+
+
+
+
+
 
     createPost();
 }
