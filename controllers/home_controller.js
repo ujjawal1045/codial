a5eb81dd985c9a98df7924602696d61222fbbaeb
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2022-03-23 22:07:10 +0530

    converted our code no async await to look more cleaner

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,31 +1,20 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function(req, res) {
-    // console.log(req.cookies);
-    // res.cookie
-    
-    // ('user_id', 25);
+module.exports.home = async function(req, res) {
+    try {
+            //populate the user of each post
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
+        let users = await User.find({});
 
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: "Codeial | home",
-    //         posts: posts
-    //     });
-    // });
-//populate the user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
-
-
-        User.find({}, function(err,users) {
+        
 
             return res.render('home', {
                 title: "Codeial | home",
@@ -33,10 +22,10 @@ module.exports.home = function(req, res) {
                 all_users: users
             });
 
-        });
-        
-    })
-    
-    //return res.end('<h1> Express is up for codeial! </h1>')
-    
+    } catch(err) {
+        console.log('Error', err);
+        return;
+
+    }
+
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -2,37 +2,49 @@ const { remove } = require('../models/post');
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 
-module.exports.create = function(req, res) {
-      Post.create({
-        content: req.body.content,
-        user: req.user._id
-    },function(err, post) {
-        if(err) {
-            console.log('error in creating post');
-            return;
-    }
+module.exports.create = async function(req, res) {
+    try {
+        await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        });
+        
     
+        req.flash('success', 'Post published');
+        return res.redirect('back');
 
-    req.flash('success', 'Post published');
-    return res.redirect('back');
-    });
+    } catch(err) {
+        console.log('Error', err);
+        return;
+
+
+    }
+      
+    
     
 }
 
-module.exports.destroy = function(req, res) {
-    Post.findById(req.params.id, function(err, post) {
+module.exports.destroy = async function(req, res) {
+
+    try {
+        let post = await Post.findById(req.params.id); 
         //.id means converting the object id to string otherwise ._id will use for number
-        if(post.user == req.user.id) {
-            post.remove();
-            req.flash('error', 'Post deleted');
 
-            Comment.deleteMany({post: req.params.id}, function(err) {
-                return res.redirect('back');
+    if(post.user == req.user.id) {
+        post.remove();
+        req.flash('error', 'Post deleted');
 
-            });
+       await Comment.deleteMany({post: req.params.id});
+       return res.redirect('back');
 
-        } else {
-            return res.redirect('back');
-        }
-    });
+
+    } else {
+        return res.redirect('back');
+    }
+
+    }catch(err) {
+        console.log('Error', err);
+        return;
+    }
+    
 }
