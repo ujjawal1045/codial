12445262602f149e467121ae243dd21698af20df
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2022-03-23 23:48:51 +0530

    sended the data through ajax but not received it yet

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -1,24 +1,24 @@
-//const { create } = require("connect-mongo");
+
 //method to submit the form data using ajax for new post
-//{
-//     let createPost = function(){
-//         let newPostForm = $('#new-post-form');
-//         newPostForm.submit(function(e) {
-//             e.preventDefault();
+{
+    let createPost = function(){
+        let newPostForm = $('#new-post-form');
+        newPostForm.submit(function(e) {
+            e.preventDefault();
 
-//             $.ajax({
-//                 type: 'post',
-//                 url: '/post/create',
-//                 data: newPostForm.serialize(),
-//                 success: function(data) {
-//                     console.log(data);
+            $.ajax({
+                type: 'post',
+                url: '/post/create',
+                data: newPostForm.serialize(),
+                success: function(data) {
+                    console.log(data);
 
-//                 }, error: function(error) {
-//                     console.log(error.responseText);
-//                 }
-//             });
-//         });
-//     }
+                }, error: function(error) {
+                    console.log(error.responseText);
+                }
+            });
+        });
+    }
 
-//     createPost();
-// }
+    createPost();
+}
