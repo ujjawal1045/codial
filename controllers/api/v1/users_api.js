38f4d953fbd0808d73fcbc1e825ac1117b70adf2
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2022-03-27 03:18:05 +0530

    created token using jwt and that token will be use for authentication

---
+++ controllers/api/v1/users_api.js
@@ -0,0 +1,28 @@
+const User = require('../../../models/user');
+const jwt = require('jsonwebtoken');
+module.exports.createSession = async function(req, res){
+   
+    try{
+        let user = await User.findOne({email: req.body.email});
+        if(!user || user.password != req.body.password) {
+            return res.json(422, {
+                message: "invaild username or password",
+
+            });
+        }
+
+        return res.json(200, {
+            message: "signed in successfull and here is your token, please keep it safe1",
+            data: {
+                token: jwt.sign(user.toJSON(), 'codeial', {expiresIn: 10000})
+            }
+        })
+
+    }catch(err) {
+        console.log('***', err);
+        return res.json(500, {
+            message:" internal server error "
+        });
+
+    }
+ }
