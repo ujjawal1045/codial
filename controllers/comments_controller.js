4da963ce05f50b8ce6618f55d526c3f758429870
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2022-05-12 19:58:29 +0530

    created comment mailer to sent mail and modified comment controller(not a part of this mail)modified from previous

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,3 +1,36 @@
+// const { redirect } = require('express/lib/response');
+// const Comment = require('../models/comment');
+// const Post = require('../models/post');
+// module.exports.create = async function(req, res) {
+//     try {
+//         let post = await Post.findById(req.body.post);
+//             if(post) {
+//                 let comment = await Comment.create({
+//                     content: req.body.content,
+//                     post: req.body.post,
+//                     user: req.user._id
+//                 });
+                    
+                    
+                    
+//                     post.comments.push(comment);
+//                     post.save();
+//                      res.redirect('/');
+                
+//             }
+        
+
+//     }catch(err) {
+//         console.log('Error', err);
+//         return;
+
+//     }
+    
+// }
+
+
+
+
 const { redirect } = require('express/lib/response');
 const Comment = require('../models/comment');
 const Post = require('../models/post');
@@ -15,18 +48,57 @@ module.exports.create = async function(req, res) {
                     
                     post.comments.push(comment);
                     post.save();
+
+                    if(req.xhr) {
+                        comment = await comment.popilate('user', 'name').execPopulate();
+                    
+                        return res.status(200).json({
+                            data: {
+                                comment: comment
+                            }, 
+                            message: "Post created!"
+                        });
+                    }
+
+                    req.flash('success', "Comment published!");
                      res.redirect('/');
                 
             }
         
 
     }catch(err) {
-        console.log('Error', err);
+        req.flash('error', err);
         return;
 
     }
     
 }
+
+
+
+// module.exports.destroy = async function(req, res) {
+    
+//     try{
+
+//         let comment = await Comment.findById(req.params.id);
+//         if(comment.user == req.user.id) {
+//             let postId = comment.post;
+//             comment.remove();
+
+//             let post = Post.findByIdAndUpdate(postId,{$pull: {comments: req.params.id}});
+//                 return res.redirect('back');
+            
+//         } else {
+//             return res.redirect('back');
+
+//         }
+
+//     }catch(err) {
+
+//         console.log('Error', err);
+//         return;
+//     }
+// }
 
 module.exports.destroy = async function(req, res) {
     
@@ -38,9 +110,21 @@ module.exports.destroy = async function(req, res) {
             comment.remove();
 
             let post = Post.findByIdAndUpdate(postId,{$pull: {comments: req.params.id}});
+            //send the comment id which was deleted back to the views
+            if(req.xhr) {
+                return res.status(200).json({
+                    data: {
+                        comment_id : req.params.id
+
+                    },
+                    message: "Post deleted"
+                });
+            }
+            req.flash('success','Comment deleted!');
                 return res.redirect('back');
             
         } else {
+            req.flash('error', 'unauthorised');
             return res.redirect('back');
 
         }
---
+++ mailers/comments_maielr.js
@@ -0,0 +1,21 @@
+const nodeMailer = require('../config/nodemailer');
+
+
+//another way of exporting a method
+exports.newComment = (comment)=> {
+    console.log('inside newComment mailer');
+    nodeMailer.transporter.sendMail({
+        from: 'ujjawals286@gmail.com',
+        to: comment.user.email,
+        subject: "New comment on your post",
+        html: <h1> Yup, your comment is now published!</h1>
+    }, (err, info) {
+        if(err) {
+            console.log('error in sending the email',err);
+            return;
+        }
+
+        console.log('Comment successful', onfo);
+        return;
+    });
+}
